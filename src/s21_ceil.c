2f5ad5f6cd2eb3ec3cb0d84b776a0106cc8fdc38
Lawnties <99724334+tinskrin@users.noreply.github.com>
2023-06-30 19:37:05 +0300

    Kak hochesh (#4)
    Co-authored-by: Lawn Tiesha <lawnties@mi-h4.msk.21-school.ru>
    Co-authored-by: Lawn Tiesha <lawnties@mi-g5.msk.21-school.ru>

---
+++ src/s21_ceil.c
@@ -0,0 +1,13 @@
+#include "math.h"
+
+long double s21_ceil(double x) {
+    long double res = 0;
+    if (x < 0) {
+        res = (int)-x;
+        res = -res;
+    } else {
+        res = (int)x;
+        res += 1;
+    }
+    return res;
+}
